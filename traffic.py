e1ac1ca53d95eb253498f4b6abb6fe1f5da18cfb
Anton Belodedenko <anton.belodedenko@gmail.com>
2016-01-25 18:40:37 +0000

    Fixed influxdb sflow data format

--- traffic.py
+++ traffic.py
@@ -38,44 +38,54 @@ def main(cmd=None, host=None, port=None):
                      'ifOutErrors',
                      'ifPromiscuousMode']
 
-        if l[0] in 'FLOW':
-            names = ['inputPort',
-                     'outputPort',
-                     'src_MAC',
-                     'dst_MAC',
-                     'ethernet_type',
-                     'in_vlan',
-                     'out_vlan',
-                     'src_IP',
-                     'dst_IP',
-                     'IP_protocol',
-                     'ip_tos',
-                     'ip_ttl',
-                     'tcp_udp_src_port_icmp_code',
-                     'tcp_udp_dst_port_icmp_code',
-                     'tcp_flags',
-                     'packet_size',
-                     'IP_size',
-                     'sampling_rate']
+##        if l[0] in 'FLOW':
+##            names = ['inputPort',
+##                     'outputPort',
+##                     'src_MAC',
+##                     'dst_MAC',
+##                     'ethernet_type',
+##                     'in_vlan',
+##                     'out_vlan',
+##                     'src_IP',
+##                     'dst_IP',
+##                     'IP_protocol',
+##                     'ip_tos',
+##                     'ip_ttl',
+##                     'tcp_udp_src_port_icmp_code',
+##                     'tcp_udp_dst_port_icmp_code',
+##                     'tcp_flags',
+##                     'packet_size',
+##                     'IP_size',
+##                     'sampling_rate']
             
         for name in names: fields.append(name)
         d = dict(zip(fields, l))
 
         l = []
-        for k,v in d.iteritems():
-            if k not in ('sample', 'agent'):
-                json_body = {
-                    'measurement': k,
-                    'tags': {
-                        'sample': d['sample'],
-                        'agent': d['agent']                    
-                    },
-                    'time': t,
-                    'fields': {
-                        'value': v
-                    }
+        for k in ['ifInOctets', 'ifInUcastPkts', 'ifInMulticastPkts',
+                  'ifInBroadcastPkts', 'ifInDiscards', 'ifInErrors',
+                  'ifInUnknownProtos', 'ifOutOctets', 'ifOutUcastPkts',
+                  'ifOutMulticastPkts', 'ifOutBroadcastPkts', 'ifOutDiscards',
+                  'ifOutErrors']:
+            
+            json_body = {
+                'measurement': k,
+                'tags': {
+                    'sample': d['sample'],
+                    'agent': d['agent'],
+                    'ifIndex': d['ifIndex'],
+                    'ifType': d['ifType'],
+                    'ifSpeed': d['ifSpeed'],
+                    'ifDirection': d['ifDirection'],
+                    'ifStatus': d['ifStatus'],
+                    'ifPromiscuousMode': d['ifPromiscuousMode']                       
+                },
+                'time': t,
+                'fields': {
+                    'value': d[k]
                 }
-                l.append(json_body)
+            }
+            l.append(json_body)
 
         print('Write points: {0}'.format(l))
         client.write_points(l)

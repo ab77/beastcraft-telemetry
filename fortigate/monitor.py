4c9cd3d8889bf6bbd7df52f50cb56336b280f3ab
Anton Belodedenko <anton.belodedenko@gmail.com>
2016-03-27 16:39:45 +0100

    Added FortiGate interface fail-over

--- fortigate/monitor.py
+++ fortigate/monitor.py
@@ -4,21 +4,65 @@ from subprocess import Popen, PIPE
 from time import sleep
 from fcntl import fcntl, F_GETFL, F_SETFL
 from os import O_NONBLOCK, read
+import argparse
 
-# run the shell as a subprocess:
-p = Popen(['ssh', '%s'],
-        stdin = PIPE, stdout = PIPE, stderr = PIPE, shell = False)
-# set the O_NONBLOCK flag of p.stdout file descriptor:
-flags = fcntl(p.stdout, F_GETFL) # get current p.stdout flags
-fcntl(p.stdout, F_SETFL, flags | O_NONBLOCK)
-# get the output
-while True:
-    # issue command:
-    p.stdin.write('diag sys link-monitor status wifi\n')
-    # let the shell output the result:
-    sleep(2)
-    try:
-        print read(p.stdout.fileno(), 1024),
-    except OSError:
-        # the os throws an exception if there is no data
-        sleep(10)
+
+def set_defaut_route(p=None, iface='wifi'):
+    p.stdin.write('configure router static\nedit 1\nset device %s\nend\n' % iface)
+
+    
+def main(user='admin', host=None, port=22, iface=None, backup=None):
+    # assume interface is online
+    iface_status = 'alive'
+    # run the shell as a subprocess:
+    p = Popen(['ssh', '%s@%s' % (user, host)],
+              stdin = PIPE, stdout = PIPE, stderr = PIPE, shell = False)
+    # set the O_NONBLOCK flag of p.stdout file descriptor:
+    flags = fcntl(p.stdout, F_GETFL) # get current p.stdout flags
+    fcntl(p.stdout, F_SETFL, flags | O_NONBLOCK)
+    # get the output
+    while True:
+        # issue command:
+        p.stdin.write('diag sys link-monitor status %s\n' % iface)
+        # let the shell output the result:
+        sleep(5)
+        try:
+            line = read(p.stdout.fileno(), 1024)
+            if line.split()[0] == 'Link':
+                print '%s %s %s' % (line.split()[2], line.split()[3], line.split()[4])
+                if line.split()[4] == 'die' and iface_status == 'alive':
+                    iface_status = line.split()[4]
+                    print 'set default route to %s\n' % backup
+                    set_defaut_route(p=p, iface=backup)
+                if line.split()[4] == 'alive' and iface_status == 'die':
+                    print 'set default route to %s\n' % iface
+                    set_defaut_route(p=p, iface=iface)
+                    iface_status = line.split()[4]
+                    
+        except OSError:
+            # the os throws an exception if there is no data
+            sleep(5)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='FortiGate interface monitor')
+    parser.add_argument('--host', type=str, required=True, default=None,
+                        help='FortiGate appliance hostname or IP')
+    parser.add_argument('--port', type=int, required=False, default=22,
+                        help='SSH port of the FortiGate appliance')
+    parser.add_argument('--user', type=str, required=False, default='admin',
+                        help='FortiGate admin username')
+    parser.add_argument('--iface', type=str, required=True, default=None,
+                        help='FortiGate interface to monitor (e.g. wifi)')
+    parser.add_argument('--backup', type=str, required=True, default=None,
+                        help='FortiGate interface to fail-over to (e.g. wan2)')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(user=args.user,
+         host=args.host,
+         port=args.port,
+         iface=args.iface,
+         backup=args.backup)
